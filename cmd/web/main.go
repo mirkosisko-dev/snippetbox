f2bff5d2a15f7620c909f3e838f21d5377ae90e9
mirkosisko-dev <mirkosisko.dev@gmail.com>
2025-02-19 20:21:22 +0100

    chapter 1-3

---
+++ cmd/web/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	logger *slog.Logger
+}
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		logger: logger,
+	}
+
+	logger.Info("starting server", "addr", *addr)
+
+	err := http.ListenAndServe(*addr, app.routes())
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
