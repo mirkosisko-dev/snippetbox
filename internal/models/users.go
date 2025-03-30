b3f03c2310348bbd7d5155dc3d7fda951682b425
mirkosisko-dev <mirkosisko.dev@gmail.com>
2025-03-30 15:45:16 +0200

    chapter 11 - Request Context

--- internal/models/users.go
+++ internal/models/users.go
@@ -71,5 +71,14 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 }
 
 func (m *UserModel) Exists(id int) (bool, error) {
-	return false, nil
+	var exists bool
+
+	stmt := "SELECT EXISTS(SELECT true FROM users WHERE id = ?)"
+
+	err := m.DB.QueryRow(stmt, id).Scan(&exists)
+	if err != nil {
+		return false, err
+	}
+
+	return exists, err
 }
